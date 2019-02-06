e0a3dd2b3392b7491b0ec88db8f999d1777e6ba9
Henrik <lordminning@gmail.com>
2019-02-06 15:25:33 +0100

    Setup of environment for visualisation

---
+++ Assets/Scripts/Angle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angle : MonoBehaviour {
+
+    public Transform target;
+    private double angle;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        angle = Vector3.Angle(transform.position, target.position);
+        Debug.Log(angle);
+	}
+}
