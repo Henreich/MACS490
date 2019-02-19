3a028237d26dc8e0c1d99573f3cfbb839e6b1ab7
Henrik Solum <henrik.haugom.solum@gmail.com>
2019-02-19 10:08:50 +0100

    Added SteamVR plugin

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Controller : MonoBehaviour { 
+    public XRNode NodeType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = InputTracking.GetLocalPosition(NodeType);
+        transform.localRotation = InputTracking.GetLocalRotation(NodeType);
+
+        Debug.Log(InputTracking.GetLocalPosition(NodeType));
+    }
+}
