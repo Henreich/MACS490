44c3c3eebc2e804c030c9cabf7f4c8c6e07646ad
SchoolDev <jesushl@stud.ntnu.no>
2019-02-19 12:54:32 +0100

    Add basic functionality to the controller
    Added the basic mapping of input for the participants in the experiment to control
    different variables. Very rough as it is right now, but a good foundation for future
    tuning.

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,22 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using Valve.VR;
+using TMPro;
 
 public class Controller : MonoBehaviour { 
-    public XRNode NodeType;
+    //public XRNode NodeType;
+
+    public SteamVR_Action_Single squeezeAction;
+    public SteamVR_Action_Vector2 touchpadAction;
+
+    public TextMeshPro text;
+    private double MINIMUM_FONT_SIZE = 1.0f;
+    private double MAXIMUM_FONT_SIZE = 7.0f;
+    private double TRIGGER_THRESHOLD = 0.85f;
+    private float TEXT_SIZING_INCREMENTS = 0.01f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //text = GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = InputTracking.GetLocalPosition(NodeType);
-        transform.localRotation = InputTracking.GetLocalRotation(NodeType);
+        if (SteamVR_Actions._default.InteractUI.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            Vector2 touchpadValue = touchpadAction.GetAxis(SteamVR_Input_Sources.Any);
+            //print(touchpadValue);
 
-        Debug.Log(InputTracking.GetLocalPosition(NodeType));
+            // Where the touch pad was triggered
+            if (touchpadValue.x > TRIGGER_THRESHOLD)
+            {
+                print("right pad triggered");
+                text.fontSize += TEXT_SIZING_INCREMENTS;
+                
+            }
+            else if (touchpadValue.x < -TRIGGER_THRESHOLD)
+            {
+                print("left pad triggered");
+                text.fontSize -= TEXT_SIZING_INCREMENTS;
+
+            } else if (touchpadValue.y > TRIGGER_THRESHOLD)
+            {
+                print("up pad triggered");
+            } else if (touchpadValue.y < -TRIGGER_THRESHOLD)
+            {
+                print("down pad triggered");
+            }
+        }
+
+        //float triggerValue = squeezeAction.GetAxis(SteamVR_Input_Sources.Any);
+
+        //if (triggerValue > 0.0f)
+        //{
+        //    print(triggerValue);
+        //}
+
+        if(SteamVR_Actions._default.GrabPinch.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            //print("Trigger down.");
+            print(text.fontSize);
+        }
+
     }
 }
