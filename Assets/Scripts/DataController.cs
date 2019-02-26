d3e7821167587e1bd6c804ab06abb7cae936e422
Henrik Solum <henrik.haugom.solum@gmail.com>
2019-02-26 13:47:02 +0100

    End of day commit
    Not working parser, currently rewriting it because an example
    I used did not work as far as I can tell.

---
+++ Assets/Scripts/DataController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataController : MonoBehaviour
+{
+    private string TEXT_DATA_FILE_NAME = "text_data.json";
+
+    //private TextData[] allTextData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadTextData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            print(allTextData);
+        }
+    }
+
+    void LoadTextData()
+    {
+        TextData textData = new TextData();
+        textData.Text = "Lorem ipsum";
+
+        //string json = JsonUtility.ToJson
+
+        string filePath = Path.Combine(Application.streamingAssetsPath, TEXT_DATA_FILE_NAME);
+
+
+        //if (File.Exists(filePath))
+        //{
+        //    string fileData = File.ReadAllText(filePath);
+        //    TextCollection collection = JsonUtility.FromJson<TextCollection>(fileData);
+        //    allTextData = collection.allTextData;
+
+        //    print(allTextData.Length);
+        //    // alltextData[0].Text == Null even though allTextData.Length == 2...
+        //    print(allTextData[0]);
+        //    print(allTextData[0].Text);
+        //}
+        //else
+        //{
+        //    Debug.LogError("Cannot load text data from " + filePath);
+        //}
+    }
+
+    private class TextData
+    {
+        public string Text;
+    }
+}
