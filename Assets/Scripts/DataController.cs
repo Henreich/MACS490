fbda347f64dbca39282cdfbe490b480313cf1b07
Henrik Solum <henrik.haugom.solum@gmail.com>
2019-02-28 10:43:44 +0100

    Add write and read functionality of json to and from file.
    This update makes it possible to write to and read from JSON files. Initially
    this was recommended to be done through the use of UnityEngine.Jsonutilities, but
    it seems like there is no support for lists/arrays yet. Through the power
    of stackoverflow a helper class was found that manages this for me.
    Writing to file will most likely not be used, but is kept in case
    it becomes relevant when test data will be written to file.
    As of now all the json handling is kept within the DataController.

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class DataController : MonoBehaviour
 {
-    private string TEXT_DATA_FILE_NAME = "text_data.json";
+    private readonly string TEXT_DATA_FILE_NAME = "text_data.json";
 
-    //private TextData[] allTextData;
+    private TextData[] allTextData;
 
     // Start is called before the first frame update
     void Start()
@@ -23,46 +23,91 @@ public class DataController : MonoBehaviour
         }
     }
 
+    /**
+     * Load JSON from file and store it in the allTextData array for later access.
+     ***/
     void LoadTextData()
     {
-  
-        TextData textData = new TextData();
-        textData.Text = "Lorem ipsum";
-
-        string json = JsonUtility.ToJson(textData);
-
-        //TextData loadedData = JsonUtility.FromJson<TextData>(json);
-        //print(loadedData.Text);
-
         string filePath = Path.Combine(Application.streamingAssetsPath, TEXT_DATA_FILE_NAME);
-        string dataFromFile = File.ReadAllText(filePath);
-        TextData loadedData = JsonUtility.FromJson<TextData>(dataFromFile);
-
-        print(loadedData.Text);
         
-        //print(Application.dataPath + "/Scripts/test.json");
-        //File.WriteAllText(Application.dataPath + "/Scripts/test.json", json);
-
-
-        //if (File.Exists(filePath))
-        //{
-        //    string fileData = File.ReadAllText(filePath);
-        //    TextCollection collection = JsonUtility.FromJson<TextCollection>(fileData);
-        //    allTextData = collection.allTextData;
-
-        //    print(allTextData.Length);
-        //    // alltextData[0].Text == Null even though allTextData.Length == 2...
-        //    print(allTextData[0]);
-        //    print(allTextData[0].Text);
-        //}
-        //else
-        //{
-        //    Debug.LogError("Cannot load text data from " + filePath);
-        //}
+        if (File.Exists(filePath))
+        {
+            string fileData = File.ReadAllText(filePath);
+            allTextData = JsonHelper.FromJson<TextData>(fileData);
+        }
+        else
+        {
+            Debug.LogError("Cannot load text data from " + filePath);
+        }
     }
 
+    /**
+     * Creates dummy JSON formatted file with 4 array objects.
+     **/
+    public void CreateDummyData()
+    {
+        //TextCollection collection = new TextCollection();
+        //collection.allTextData.Add(new TextData(0, "Lorem ipsum"));
+        //collection.allTextData.Add(new TextData(1, "I can't believe it's not lorem ipsum!"));
+        //collection.allTextData.Add(new TextData(2, "Superhot is pretty fun"));
+        //collection.allTextData.Add(new TextData(3, "All good things are three"));
+
+        //TextData[] arrayToJson = collection.allTextData.ToArray();
+
+        //string json = JsonHelper.ToJson(arrayToJson, true);
+        //File.WriteAllText(Application.dataPath + "/Scripts/test.json", json);
+    }
+
+    [System.Serializable]
+    private class TextCollection
+    {
+        public List<TextData> allTextData = new List<TextData>();
+    }
+    [System.Serializable]
     private class TextData
     {
+        public int Id;
         public string Text;
+
+        public TextData(int id, string text)
+        {
+            this.Id = id;
+            this.Text = text;
+        }
+    }
+
+    /**
+     * Helper class to convert to and from arrays of objects to JSON arrays, 
+     * since this does not seem to be supportedby Jsonutilities yet. 
+     * https://stackoverflow.com/questions/36239705/serialize-and-deserialize-json-and-json-array-in-unity
+     * http://www.boxheadproductions.com.au/deserializing-top-level-arrays-in-json-with-unity/
+     * **/
+    public static class JsonHelper
+    {
+        public static T[] FromJson<T>(string json)
+        {
+            Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
+            return wrapper.Items;
+        }
+
+        public static string ToJson<T>(T[] array)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper);
+        }
+
+        public static string ToJson<T>(T[] array, bool prettyPrint)
+        {
+            Wrapper<T> wrapper = new Wrapper<T>();
+            wrapper.Items = array;
+            return JsonUtility.ToJson(wrapper, prettyPrint);
+        }
+
+        [System.Serializable]
+        private class Wrapper<T>
+        {
+            public T[] Items;
+        }
     }
 }
