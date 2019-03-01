8eacfc368f135519bcc4e0ea31317829632c8b03
Henrik Haugom Solum <henrihso@stud.ntnu.no>
2019-03-01 14:17:06 +0100

    Scaling vision does not take reading task into consideration and I got sidetracked.
    After experiencing how the dimensions fit into my vision in virtual reality,
    I come to the realisation that what I've been looking at for reference points
    were the comfortable range of human vision. This does not take into consideration
    that reading should be done at the edges of human vision. Although it might be
    comfortable to move your eyes 30 degrees to the left, it does not mean that
    you can comfortably read at such an angle as well. Further literature needs to
    be reviewed to find a good size of the screen.
    
    The sidetrack comes from being able to scale the text. I should not have started
    to tinker with this in this branch. I have however made it a bit easier to scale
    the text while in virtual reality, where text size increases based on touch and
    not touchpad presses. The closer to the edges the person in VR touches the touchpad,
    the faster the text scales up and down.

--- Assets/Scripts/ScaleManager.cs
+++ Assets/Scripts/ScaleManager.cs
@@ -7,35 +7,40 @@ public class ScaleManager : MonoBehaviour
     private Transform screen;
     private readonly float WORLD_CENTER = 0.0f;
     private readonly float SCALE_Z = 0.001f;
+    // Comfortable movement of the eyes.
+    // TODO: Is this really valid for reading though?
     private readonly float ANGLE_COMFORTABLE_LEFTRIGHT = 30;
     private readonly float ANGLE_COMFORTABLE_UP = 20;
     private readonly float ANGLE_COMFORTABLE_DOWN = 12;
 
-    private readonly float ANGLE_MAX_LEFTRIGHT = 55;
-    private readonly float ANGLE_MAX_UP = 60;
-    private readonly float ANGLE_MAX_DOWN = 40;
 
     // Start is called before the first frame update
     void Start()
     {
         screen = GetComponent<Transform>();
-        Scale();
+        ScaleToMaxVisionDimensions();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Scale();
+        //Scale();
     }
 
-    private void Scale()
+
+    /**
+     * Scales the "screen" to the max height and width based on the comfortable range
+     * of human vision of 30 degrees left and right, 20 degrees up and 12 degrees down.
+     **/
+    private void ScaleToMaxVisionDimensions()
     {
         float ScaleX    = 2 * (GetZDistanceFromZero() / Mathf.Cos(ANGLE_COMFORTABLE_LEFTRIGHT * Mathf.Deg2Rad));
         float up        = GetZDistanceFromZero() / Mathf.Cos(ANGLE_COMFORTABLE_UP * Mathf.Deg2Rad);
         float down      = GetZDistanceFromZero() / Mathf.Cos(ANGLE_COMFORTABLE_DOWN * Mathf.Deg2Rad);
         float ScaleY    = up + down;
         screen.transform.localScale = new Vector3(ScaleX, ScaleY, SCALE_Z);
+        //screen.transform.localScale = new Vector3(0.5773503f, 0.5216296f, SCALE_Z);
     }
 
     /**
