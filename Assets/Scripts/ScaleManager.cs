08e46c9d001be97442145b83133135d5ceb47648
Henrik Solum <henrik.haugom.solum@gmail.com>
2019-03-01 10:34:42 +0100

    Add basic scaling of the screen based on distance away from center.
    Using the "comfortable" viewing angles of people's eyes (approximated to be
    about 30 degrees left and right, 20 degrees up and 12 degrees down) to scale
    the screen based on how far away it is from the world center.
    Some basic trigonometry is applied to calculate how big the screen needs to be
    based on these viewing angles at the given distance to the screen.
    Currently calculated in update, but can probably only calculated whenever
    a the screen moves.

---
+++ Assets/Scripts/ScaleManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleManager : MonoBehaviour
+{
+    private Transform screen;
+    private readonly float WORLD_CENTER = 0.0f;
+    private readonly float SCALE_Z = 0.001f;
+    private readonly float ANGLE_COMFORTABLE_LEFTRIGHT = 30;
+    private readonly float ANGLE_COMFORTABLE_UP = 20;
+    private readonly float ANGLE_COMFORTABLE_DOWN = 12;
+
+    private readonly float ANGLE_MAX_LEFTRIGHT = 55;
+    private readonly float ANGLE_MAX_UP = 60;
+    private readonly float ANGLE_MAX_DOWN = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screen = GetComponent<Transform>();
+        Scale();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scale();
+    }
+
+    private void Scale()
+    {
+        float ScaleX    = 2 * (GetZDistanceFromZero() / Mathf.Cos(ANGLE_COMFORTABLE_LEFTRIGHT * Mathf.Deg2Rad));
+        float up        = GetZDistanceFromZero() / Mathf.Cos(ANGLE_COMFORTABLE_UP * Mathf.Deg2Rad);
+        float down      = GetZDistanceFromZero() / Mathf.Cos(ANGLE_COMFORTABLE_DOWN * Mathf.Deg2Rad);
+        float ScaleY    = up + down;
+        screen.transform.localScale = new Vector3(ScaleX, ScaleY, SCALE_Z);
+    }
+
+    /**
+     * Get amount of distance units between the object and the center (0, 0, 0)
+     **/
+    private float GetZDistanceFromZero()
+    {
+        return System.Math.Abs(screen.transform.localPosition.z - WORLD_CENTER);
+    }
+}
