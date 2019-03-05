3301e760280c6a58d16954f768d03172b1dea591
= <=>
2019-03-05 12:52:03 +0100

    Added the original form of the script again.
    Seems like the curve worked "as expected", producing a full curve in the original.
    Problem however is that the difference between the original implmentation and the one
    I have been attempting is that the curve is duplicated in the original, meaning the
    curve is still cut off half way through. Duplicating the curve made it seem like the
    entire curve was being used to draw the text on...

--- Assets/Scripts/OriginalVertexAttributeModifier.cs
+++ Assets/Scripts/OriginalVertexAttributeModifier.cs
@@ -2,28 +2,19 @@
 using System.Collections;
 using TMPro;
 
-
-/**
- * Initially from this forum thread.
- * http://digitalnativestudios.com/forum/index.php?topic=998.0
- */
-public class WarpTextExample : MonoBehaviour
+public class OriginalVertexAttributeModifier : MonoBehaviour
 {
 
-    private TextMeshPro m_TextComponent;
+    private TMP_Text m_TextComponent;
 
-    public AnimationCurve VertexCurve = new AnimationCurve( new Keyframe(0.0f, 0.0f),
-                                                            new Keyframe(1.0f, 1.0f),
-                                                            new Keyframe(2.0f, 0.0f)
-                                                            );
+    public AnimationCurve VertexCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(0.25f, 2.0f), new Keyframe(0.5f, 0), new Keyframe(0.75f, 2.0f), new Keyframe(1, 0f));
     public float AngleMultiplier = 1.0f;
     public float SpeedMultiplier = 1.0f;
     public float CurveScale = 1.0f;
-    public float TextAngle = 0;
 
     void Awake()
     {
-        m_TextComponent = gameObject.GetComponent<TextMeshPro>();
+        m_TextComponent = gameObject.GetComponent<TMP_Text>();
     }
 
 
@@ -35,10 +26,9 @@ public class WarpTextExample : MonoBehaviour
 
     private AnimationCurve CopyAnimationCurve(AnimationCurve curve)
     {
-        AnimationCurve newCurve = new AnimationCurve
-        {
-            keys = curve.keys
-        };
+        AnimationCurve newCurve = new AnimationCurve();
+
+        newCurve.keys = curve.keys;
 
         return newCurve;
     }
@@ -61,7 +51,6 @@ public class WarpTextExample : MonoBehaviour
 
         m_TextComponent.havePropertiesChanged = true; // Need to force the TextMeshPro Object to be updated.
         CurveScale *= 10;
-        TextAngle *= 1;
         float old_CurveScale = CurveScale;
         AnimationCurve old_curve = CopyAnimationCurve(VertexCurve);
 
@@ -89,8 +78,8 @@ public class WarpTextExample : MonoBehaviour
 
             float boundsMinX = m_TextComponent.textInfo.meshInfo[0].mesh.bounds.min.x;
             float boundsMaxX = m_TextComponent.textInfo.meshInfo[0].mesh.bounds.max.x;
-            //print("boundsMinX | maxX ==>" + boundsMinX + " | " + boundsMaxX);
-           
+
+
             for (int i = 0; i < characterCount; i++)
             {
                 if (!textInfo.characterInfo[i].isVisible)
@@ -114,45 +103,20 @@ public class WarpTextExample : MonoBehaviour
                 vertices[vertexIndex + 3] += -offsetToMidBaseline;
 
                 // Compute the angle of rotation for each character based on the animation curve
-
-                // x = TIME   = X-axis
-                // y = Height = Y-axis
                 float x0 = (offsetToMidBaseline.x - boundsMinX) / (boundsMaxX - boundsMinX); // Character's position relative to the bounds of the mesh.
-                float x1 = x0 + 0.0001f; // Magic number..?
-                float y0 = VertexCurve.Evaluate(x0) * CurveScale; // Evaluate the VertexCurve at the time (float) x0
+                float x1 = x0 + 0.0001f;
+                float y0 = VertexCurve.Evaluate(x0) * CurveScale;
                 float y1 = VertexCurve.Evaluate(x1) * CurveScale;
-
-                print(VertexCurve.Evaluate(1.0f));
 
                 Vector3 horizontal = new Vector3(1, 0, 0);
                 //Vector3 normal = new Vector3(-(y1 - y0), (x1 * (boundsMaxX - boundsMinX) + boundsMinX) - offsetToMidBaseline.x, 0);
-                // Tangent should be indicating just how much a letter needs to be rotated in order to look good on the curve.
-                // Tangent line that intercepts curve in a single spot..?
                 Vector3 tangent = new Vector3(x1 * (boundsMaxX - boundsMinX) + boundsMinX, y1) - new Vector3(offsetToMidBaseline.x, y0);
-                //print("Tangent " + tangent.x + " | " + tangent.y + " | " + tangent.z);
 
-                // Magic number here comes from hardcoding a value that makes the letter make sense with the curve displayed in the
-                // unmodified example. In this case we need to make it work with a round curve in the Z-axis.
                 float dot = Mathf.Acos(Vector3.Dot(horizontal, tangent.normalized)) * 57.2957795f;
                 Vector3 cross = Vector3.Cross(horizontal, tangent);
-
-                //if (i == 0)
-                //{
-                //    //print("cross " + cross + " | " + horizontal + " | " + tangent);
-                //    print("cross " + i + "(" + cross.x + " | " + cross.y + " | " + cross.z + ")");
-                //    print("tangent" + i + "(" + tangent.x + " | " + tangent.y + " | " + tangent.z +")");
-                //}
-                // Angle: The angle of which each individual character should be rotated along the Y-axis
                 float angle = cross.z > 0 ? dot : 360 - dot;
 
-                //print(angle + " | " + cross.z + " | " + dot);
-                //matrix = Matrix4x4.TRS(new Vector3(0, y0, 0), Quaternion.Euler(0, 0, angle), Vector3.one);
-                //print(angle);
-                matrix = Matrix4x4.TRS(new Vector3(0, 0, y0), Quaternion.Euler(0, TextAngle, 0), Vector3.one); // Z "rotation"
-                // TRANSLATION/POSITION = Placement along the Z-axis when (0, 0, VALUE)
-                // ROTATION = Rotation along the Y-axis (0, Value, 0)
-                // SCALE = No change needed.
-
+                matrix = Matrix4x4.TRS(new Vector3(0, 0, y0), Quaternion.Euler(0, 0, 0), Vector3.one);
 
                 vertices[vertexIndex + 0] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 0]);
                 vertices[vertexIndex + 1] = matrix.MultiplyPoint3x4(vertices[vertexIndex + 1]);
@@ -169,8 +133,7 @@ public class WarpTextExample : MonoBehaviour
             // Upload the mesh with the revised information
             m_TextComponent.UpdateVertexData();
 
-            //yield return new WaitForSeconds(0.025f);
-            yield return null;
+            yield return new WaitForSeconds(0.025f);
         }
     }
 
