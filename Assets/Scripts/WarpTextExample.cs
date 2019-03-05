28154ddb578375fd427580a77bf9870419bab328
= <=>
2019-03-05 12:09:05 +0100

    Lunch commit.
    Only a bunch of debug messages as I try to decipher the example code provided.

--- Assets/Scripts/WarpTextExample.cs
+++ Assets/Scripts/WarpTextExample.cs
@@ -112,30 +112,41 @@ public class WarpTextExample : MonoBehaviour
                 vertices[vertexIndex + 3] += -offsetToMidBaseline;
 
                 // Compute the angle of rotation for each character based on the animation curve
+
+                // x = TIME   = X-axis
+                // y = Height = Y-axis
                 float x0 = (offsetToMidBaseline.x - boundsMinX) / (boundsMaxX - boundsMinX); // Character's position relative to the bounds of the mesh.
                 float x1 = x0 + 0.0001f; // Magic number..?
-                float y0 = VertexCurve.Evaluate(x0) * CurveScale;
+                float y0 = VertexCurve.Evaluate(x0) * CurveScale; // Evaluate the VertexCurve at the time (float) x0
                 float y1 = VertexCurve.Evaluate(x1) * CurveScale;
+
+                print(VertexCurve.Evaluate(1.0f));
 
                 Vector3 horizontal = new Vector3(1, 0, 0);
                 //Vector3 normal = new Vector3(-(y1 - y0), (x1 * (boundsMaxX - boundsMinX) + boundsMinX) - offsetToMidBaseline.x, 0);
                 // Tangent should be indicating just how much a letter needs to be rotated in order to look good on the curve.
                 // Tangent line that intercepts curve in a single spot..?
                 Vector3 tangent = new Vector3(x1 * (boundsMaxX - boundsMinX) + boundsMinX, y1) - new Vector3(offsetToMidBaseline.x, y0);
-                print("Tangent " + tangent.x + " | " + tangent.y + " | " + tangent.z);
+                //print("Tangent " + tangent.x + " | " + tangent.y + " | " + tangent.z);
 
                 // Magic number here comes from hardcoding a value that makes the letter make sense with the curve displayed in the
                 // unmodified example. In this case we need to make it work with a round curve in the Z-axis.
                 float dot = Mathf.Acos(Vector3.Dot(horizontal, tangent.normalized)) * 57.2957795f;
                 Vector3 cross = Vector3.Cross(horizontal, tangent);
+
+                //if (i == 0)
+                //{
+                //    //print("cross " + cross + " | " + horizontal + " | " + tangent);
+                //    print("cross " + i + "(" + cross.x + " | " + cross.y + " | " + cross.z + ")");
+                //    print("tangent" + i + "(" + tangent.x + " | " + tangent.y + " | " + tangent.z +")");
+                //}
+                // Angle: The angle of which each individual character should be rotated along the Y-axis
                 float angle = cross.z > 0 ? dot : 360 - dot;
 
-                //print(dot);
-                //print(cross);
-                //print(angle);
+                //print(angle + " | " + cross.z + " | " + dot);
                 //matrix = Matrix4x4.TRS(new Vector3(0, y0, 0), Quaternion.Euler(0, 0, angle), Vector3.one);
                 //print(angle);
-                matrix = Matrix4x4.TRS(new Vector3(0, 0, y0), Quaternion.Euler(0, -90, 0), Vector3.one); // Z "rotation"
+                matrix = Matrix4x4.TRS(new Vector3(0, 0, y0), Quaternion.Euler(0, tangent.z, 0), Vector3.one); // Z "rotation"
                 // TRANSLATION/POSITION = Placement along the Z-axis when (0, 0, VALUE)
                 // ROTATION = Rotation along the Y-axis (0, Value, 0)
                 // SCALE = No change needed.
