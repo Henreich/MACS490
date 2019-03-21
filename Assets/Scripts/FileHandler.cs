97c376e98e72187aeba45a40ade26e35f9e92e4d
Henrik Solum <henrik.haugom.solum@gmail.com>
2019-03-21 17:04:43 +0100

    Add basic file I/O for writing experiment data
    Currently made it so that a file is created with a number
    that increments each time the file is marked as completed i.e
    the participant has finished the experiment.
    
    End of day commit.

---
+++ Assets/Scripts/FileHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileHandler : MonoBehaviour
+{
+    private string path = "Assets/Resources/";
+    private string fileName = "participant";
+    private int lastParticipantId = 0;
+    private readonly string suffix = ".txt";
+
+    void Start()
+    {
+        WriteToFile("Participant: " + lastParticipantId + "\n");
+    }
+
+    private void WriteToFile(string text)
+    {
+        string filePath = path + fileName + lastParticipantId + suffix;
+
+        if (!File.Exists(filePath))
+        {
+            StreamWriter sw = new StreamWriter(filePath, true);
+            sw.WriteLine(text);
+            sw.Close();   
+        } else
+        {
+            lastParticipantId++;
+            WriteToFile("Participant: " + lastParticipantId + "\n");
+        }
+    }
+
+    private void FileIsComplete()
+    {
+        lastParticipantId++;
+    }
+}
